74fbd6c6b724ca8a5a1c954ab91ddff01bd19e1e
acaromartin <alvarocaromartin@gmail.com>
2025-04-25 11:07:35 +0200

    subida

---
+++ examen/p07.js
@@ -0,0 +1,8 @@
+let alumno = {
+    nombre : "Begoña",
+    nLista : 13,
+    asignatura : "ISO",
+    notas : [10,5,6,4]
+};
+
+console.log(alumno);
---
+++ examen/p08.js
@@ -0,0 +1,5 @@
+function dado10(){
+    return Math.floor(Math.random()*10)+1;
+    
+}
+console.log(dado10());
