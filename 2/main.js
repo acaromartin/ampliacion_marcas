cb947f5da7fc4f53b9856a4acce9c6d23e8cfea6
acaromartin <alvarocaromartin@gmail.com>
2025-05-12 11:17:01 +0200

    subida

--- 2/main.js
+++ 2/main.js
@@ -15,7 +15,9 @@ function init(){
         y: 150,
         w:50,
         h:50,
-        imagen: new Image()
+        imagen: new Image(),
+        es100:false,
+        es50:false
 
     };
     let bala = null;
@@ -30,7 +32,23 @@ function init(){
     }
     function pintarDiana() 
     {           
-        ctx.drawImage(diana.imagen,diana.x,diana.y,diana.w,diana.h);  
+        if (diana.es100)
+        {
+            ctx.font = '20px Arial';
+            ctx.fillStyle = 'blue';
+            ctx.fillText("100 EBLB", diana.x -25, diana.y - 25);
+        }
+        else if ((diana.es50)&& (!diana.es100))
+        {
+            ctx.font = '15px Arial';
+            ctx.fillStyle = 'red';
+            ctx.fillText("50 EBLB", diana.x -25, diana.y - 25);              
+        }
+        else if ((!diana.es50) && (!diana.es100))
+        {
+            ctx.drawImage(diana.imagen,diana.x,diana.y,diana.w,diana.h);
+        }
+
     }
 
     function pintarBala()
@@ -45,6 +63,30 @@ function init(){
             ctx.arc(bala.x ,bala.y,6,0,Math.PI*2);
             //ctx.stroke();
             ctx.fill();
+
+            // hay colisión con diana
+
+            // ES CENTRO ???????
+            if(
+                (bala.x>=diana.x+20 &&  bala.x<=diana.x+30)
+                &&
+                (bala.y>=diana.y +20 && bala.y <=  diana.y + 30)
+            )
+            {
+                console.log(100);
+                diana.es100=true;
+            }
+            else if (
+                (bala.x>=diana.x && bala.x <= diana.x + diana.w)
+                &&
+                (bala.y>=diana.y && bala.y <= diana.y + diana.h)
+            )
+            {
+                console.log(50);
+                diana.es50=true;
+            }
+
+            
 
             // si nos salimos del campo que no haya bala
             if ((existe_bala) && (bala.x>canvas.width))
@@ -81,6 +123,21 @@ function init(){
 
     } );
 
+
+    document.getElementById('b1').addEventListener('click',() =>
+    {
+        if (!existe_bala)
+        {
+            existe_bala=true;
+            bala = {
+                x:p1.x + p1.w,
+                y:p1.y,
+                velocidad:7
+            }
+
+        }    
+    }
+);
 
     function bucleJuego() 
     {
